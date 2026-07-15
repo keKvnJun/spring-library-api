e9c6d187bb2d624696c68dcd7846b322aac4b5d9
keKvnJun <7377kevin@gmail.com>
2026-07-15 19:04:42 -0300

    feat: add initial Spring Boot setup

---
+++ spring-library-api/src/test/java/com/kevin/spring_library_api/SpringLibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevin.spring_library_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
