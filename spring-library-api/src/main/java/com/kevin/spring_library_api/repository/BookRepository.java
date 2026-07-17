028bffbf0f2a38ad8f4c1cd7e942b5268e048263
keKvnJun <7377kevin@gmail.com>
2026-07-17 00:19:14 -0300

    feat: refactor BookRepository methods for author and genres; enhance query for genre filtering

--- spring-library-api/src/main/java/com/kevin/spring_library_api/repository/BookRepository.java
+++ spring-library-api/src/main/java/com/kevin/spring_library_api/repository/BookRepository.java
@@ -1,9 +1,12 @@
 package com.kevin.spring_library_api.repository;
 import com.kevin.spring_library_api.model.Book;
 import com.kevin.spring_library_api.model.Genre;
+import com.kevin.spring_library_api.model.Language;
+
 import java.util.Set;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,15 +20,26 @@ public interface BookRepository extends JpaRepository<Book, Long>{
 
     List<Book> findByTitle(String title);
 
-    Optional<Book> findByAuthor(String author);
+    List<Book> findByAuthor(String author);
 
     List<Book> findByPublisher(String publisher);
 
-    List<Book> findByGenres(Set<Genre> genres);
+    @Query("""
+        SELECT b
+        FROM Book b
+        JOIN b.genres g
+        WHERE g IN :genres
+        GROUP BY b
+        HAVING COUNT(DISTINCT g) = :genreCount
+        """)
+    List<Book> findByAllGenres(
+            @Param("genres") Set<Genre> genres,
+            @Param("genreCount") long genreCount
+    );
 
-    List<Book> findByLanguage(String language);
+    List<Book> findByLanguage(Language language);
 
-    List<Book> findByIsbn(String isbn);
+    Optional<Book> findByIsbn(String isbn);
 
     @Query("SELECT b FROM Book b WHERE b.price < :price")
     List<Book> findByPriceLessThan(BigDecimal price);
